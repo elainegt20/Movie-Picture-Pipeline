c27cbf6a9d1a7d97e1b91127d7266e7ea16818aa
elainegt <elaineblue2004@gmail.com>
2025-02-24 13:02:10 -0500

    updates

--- starter/backend/movies/resources.py
+++ starter/backend/movies/resources.py
@@ -3,6 +3,7 @@ from flask.views import MethodView
 
 # Dummy database to hold movie examples
 # Dummy database to hold movie examples
+# Dummy database to hold movie examples
 movies = {
     "123": {"title": "Top Gun: Maverick", "description": "Fighter planes"},
     "456": {"title": "Sonic the Hedgehog", "description": "Blue Sega character"},
